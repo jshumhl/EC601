b3e0ee29efccd5496ce212543aad8b6c0799ee8a
Jonathan Shum <71789623+jshumhl@users.noreply.github.com>
2023-11-03 07:07:59 -0400

    Add files via upload

--- Project2/calculate_quote.py
+++ Project2/calculate_quote.py
@@ -1,82 +1,123 @@
 from datetime import datetime
 import requests
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
+import googlemaps
 
 
 # Configurations
-config = {
-    "base_price": 10,
-    "tier_1_distance": 10,
-    "tier_1_price": 2,
-    "tier_2_distance": 30,
-    "tier_2_price": 1.5,
-    "tier_3_price": 1,
+
+general_config = {
     "weekend_multiplier": 1.2,
-    "peak_multiplier": 1.5,
     "peak_start": 8,
     "peak_end": 10,
     "evening_peak_start": 17,
     "evening_peak_end": 19,
-    "out_of_state_charge": 50,
-    "max_distance": 50
 }
 
+all_config = {
+    "restaurant": {
+        "base_price": 5,
+        "max_distance": 5,  # in miles
+        "business_address": "58 Beach St #2017, Boston, MA, 02111",
+        "opening_time": "09:00",  # 9 AM
+        "closing_time": "22:00",  # 10 PM
+        "peak_multiplier": 1.1,
+        "tier_1_distance": 0.5,
+        "tier_1_price": 0.8,
+        "tier_2_distance": 4,
+        "tier_2_price": 1,
+        "tier_3_price": 1.2,
+        "out_of_state_charge": 50,
+    },
+    "moving_company": {
+        # Configurations specific to the moving company
+    },
+    # More business types can be added with their specific configurations
+}
 
-GOOGLE_MAPS_API_KEY = "ZXZ"
+GOOGLE_MAPS_API_KEY = "AIzaSyC2YoC_ujO9bCJAVHwM6RmHcVNOTXZE8HI"
+# types: restaurant, moving_company, courier_service, 
+# Not implemented cuz im lazy af: repair_service, event_planners
+business_type = "restaurant"
+config = all_config[business_type]
+template = business_type + '.html'
 
 app = Flask(__name__)
 
-def calculate_distance(address_1, address_2=None):
+
+@app.route('/')
+def index():
+    return render_template(template)
+
+def calculate_distance(address_1, address_2=None, service_datetime=None):
     # If address_2 is not provided, use a default address (for businesses like restaurants)
     if not address_2:
         address_2 = "DEFAULT_BUSINESS_ADDRESS"
     
-    # Construct the URL for the Distance Matrix API
-    url = f"https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins={address_1}&destinations={address_2}&key={GOOGLE_MAPS_API_KEY}"
+    gmaps = googlemaps.Client(key=GOOGLE_MAPS_API_KEY)
     
-    # Make the request
-    response = requests.get(url)
-    data = response.json()
-
-    # Check if the request was successful
-    if data["status"] == "OK":
-        # Extract the distance value (in meters) from the response
-        distance_meters = data["rows"][0]["elements"][0]["distance"]["value"]
-        return distance_meters / 1000  # Convert to kilometers
+    # If service_datetime is provided, convert it to a timestamp
+    if service_datetime:
+        # Check if the service_datetime is in the past
+        if service_datetime < datetime.now():
+            raise ValueError('The service date and time must be in the future.')
+        departure_timestamp = int(service_datetime.timestamp())
     else:
-        # Handle the error accordingly
-        return None
+        departure_timestamp = None
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+    # Get directions between the two addresses
+    directions_result = gmaps.directions(
+        address_1, 
+        address_2, 
+        mode="driving", 
+        departure_time=departure_timestamp
+    )
 
-@app.route('/calculate-quote', methods=['POST'])
+    # Check the response for the 'legs' of the journey
+    if directions_result and 'legs' in directions_result[0]:
+        distance_meters = directions_result[0]['legs'][0]["distance"]["value"]
+        distance_miles = distance_meters / 1609.34  # Convert meters to miles
+
+        # Use duration in traffic if available, otherwise use standard duration
+        if 'duration_in_traffic' in directions_result[0]['legs'][0]:
+            duration_minutes = directions_result[0]['legs'][0]['duration_in_traffic']['value'] / 60
+        else:
+            duration_minutes = directions_result[0]['legs'][0]['duration']['value'] / 60
+
+        return round(distance_miles, 1), round(duration_minutes)
+    else:
+        raise ValueError('Directions request failed or returned no routes.')
+
 def calculate_cost(distance, time_of_week, out_of_state):
     # Base price
     base_price = config["base_price"]
 
     # Tiered distance-based cost
+    if "max_distance" in config:
+        if distance >= config["max_distance"] :
+            return None
+        
     if distance <= config["tier_1_distance"]:
         distance_cost = config["tier_1_price"] * distance
     elif distance <= config["tier_2_distance"]:
         distance_cost = config["tier_1_price"] * config["tier_1_distance"] + config["tier_2_price"] * (distance - config["tier_1_distance"])
     else:
         distance_cost = (config["tier_1_price"] * config["tier_1_distance"] +
-                         config["tier_2_price"] * (config["tier_2_distance"] - config["tier_1_distance"]) +
-                         config["tier_3_price"] * (distance - config["tier_2_distance"]))
+                        config["tier_2_price"] * (config["tier_2_distance"] - config["tier_1_distance"]) +
+                        config["tier_3_price"] * (distance - config["tier_2_distance"]))
+            
 
     # Time of the week multiplier
     if time_of_week == "weekend":
-        time_multiplier = config["weekend_multiplier"]
+        time_multiplier = general_config["weekend_multiplier"]
     else:
         time_multiplier = 1
 
     # Check if the current time falls within peak hours
     current_hour = datetime.now().hour
-    if (config["peak_start"] <= current_hour <= config["peak_end"] or
-        config["evening_peak_start"] <= current_hour <= config["evening_peak_end"]):
-        peak_multiplier = config["peak_multiplier"]
+    if (general_config["peak_start"] <= current_hour <= general_config["peak_end"] or
+        general_config["evening_peak_start"] <= current_hour <= general_config["evening_peak_end"]):
+        peak_multiplier = general_config["peak_multiplier"]
     else:
         peak_multiplier = 1
 
@@ -84,25 +125,36 @@ def calculate_cost(distance, time_of_week, out_of_state):
     out_of_state_charge = config["out_of_state_charge"] if out_of_state else 0
 
     # Maximum distance check
-    if config["max_distance"] > 0 and distance > config["max_distance"]:
-        return "Service not available for the specified distance."
+    #if config["max_distance"] > 0 and distance > config["max_distance"]:
+    #    return "Service not available for the specified distance."
 
     # Calculate the total cost
     total_cost = (base_price + distance_cost) * time_multiplier * peak_multiplier + out_of_state_charge
 
-    return total_cost
+    return round(total_cost, 2)
 
 
 @app.route('/calculate-quote', methods=['POST'])
 def calculate_quote():
     data = request.json
+    # personal info for future use
+    customer_name = data.get('customer_name')
+    customer_email = data.get('customer_email')
+    customer_phone = data.get('customer_phone')
     
     # Construct addresses from individual fields
-    origin_address = f"{data['origin_street']}, {data['origin_city']}, {data['origin_state']} {data['origin_zip']}"
-    dest_address = f"{data['dest_street']}, {data['dest_city']}, {data['dest_state']} {data['dest_zip']}"
+    if business_type == "restaurant":
+        origin_address = config["business_address"]
+        out_of_state = False
+    else:
+        origin_address = f"{data['origin_address']}"
+        out_of_state = data['origin_state'] != data['dest_state']
+    dest_address = f"{data['dest_address']}"
+    print(origin_address)
+    print(dest_address)
 
     # Determine out-of-state condition
-    out_of_state = data['origin_state'] != data['dest_state']
+    
     
     # Extract service date and time and determine if it's a weekday or weekend
     service_datetime_str = f"{data['service_date']} {data['service_time']}"
@@ -113,12 +165,26 @@ def calculate_quote():
         time_of_week = "weekend"
     
     # Calculate distance using Google Maps API (pseudo-code for now)
-    distance = calculate_distance(origin_address, dest_address)
+    distance, p2ptime = calculate_distance(origin_address, dest_address, service_datetime)
+    print("distance:", distance)
+    print("time:", p2ptime)
     
     # Calculate cost based on distance, time of week, and other factors
-    quote = calculate_cost(distance, time_of_week, out_of_state)
+    cost = calculate_cost(distance, time_of_week, out_of_state)
+    if cost is None:
+        quote = "The address is too far"
+        duration = ""
+    else:
+        quote = "Estimated quote is: $" + str(cost)
+        duration = 'The approximate time is ' + str(p2ptime) + ' minutes';
+    print("quote:", quote)
     
-    return jsonify({'quote': quote})
+    return jsonify({
+        'quote': quote, 
+        'time': duration
+    })
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
